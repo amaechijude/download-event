557617e0426761282b8771783c36b06013e142a6
amaechijude <ugwuamaechi5@gmail.com>
2025-07-30 23:15:38 +0100

    to fix stands next

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { DesktopHero } from "./screens/DesktopHero";
 import { AboutUs } from "./components/About/About";
+import { VisibilitySection } from "./components/VisibilitySection/VisibilitySection";
 
 createRoot(document.getElementById("app") as HTMLElement).render(
   <StrictMode>
     <DesktopHero />
     <AboutUs />
+    <VisibilitySection />
   </StrictMode>,
 );
